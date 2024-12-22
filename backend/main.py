3dc14a404bbeaba2150f790597b9991fc6df5590
winshine0326 <shane10340@gmail.com>
2024-12-23 01:21:21 +0900

    feat: openAPI 받아오기

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,10 @@ import requests
 
 app = FastAPI()
 
+url = "https://api.odcloud.kr/api/3074462/v1/uddi:161740bd-8ec5-4734-9a3d-f7a2cde34942?page=1&perPage=10&serviceKey=Pqah%2FE7nqe%2FQNOV8VamELc3HkATE6nr8RTGRTCA8qVK%2BcEQhNVedJrwZzhxXu%2B0c08Fc8SNSs84BbIEdhZz9nw%3D%3D"
+
+response = requests.get(url)
+
 app.add_middleware( #CORS 설정
     CORSMiddleware,
     allow_origins=["*"],
@@ -16,5 +20,5 @@ app.add_middleware( #CORS 설정
 
 @app.get('/')
 def read_root():
-    
-    return {"message":"success"}
+    data = response.json()
+    return {"message":"success","data":data}
